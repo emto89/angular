412ae20ea3b78ea2f7cbc21b6e51a946c7bbd898
mdiazCity <64026708+mdiazCity@users.noreply.github.com>
2021-01-28 18:22:26 -0500

    Clase 27-Enero-2021

---
+++ IONIC/01-myApp/src/app/tab2/tab2.page.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+
+  constructor() { }
+  
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    let numero = 30;
+
+    // debugger;
+
+    numero = 20;
+
+    numero = numero + 20;
+
+    numero = 0;
+
+    console.log(numero);
+  }
+
+}
